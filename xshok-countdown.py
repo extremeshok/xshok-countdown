a269e021074ad10c92efeeef7f05f0e8edf39347
eXtremeSHOK <extremeshok@users.noreply.github.com>
2018-01-22 14:09:42 +0200

    Update xshok-countdown.py

--- xshok-countdown.py
+++ xshok-countdown.py
@@ -7,7 +7,7 @@
 # Copyright (c) Adrian Jon Kriel :: admin@extremeshok.com
 ################################################################################
 #
-# Script updates can be found at: https://bitbucket.org/extremeshok/xshok-countdown
+# Script updates can be found at: https://github.com/extremeshok/xshok-countdown
 #
 # License: BSD (Berkeley Software Distribution)
 #
